15fb90b536873306a4cda9ce1240dc4b9c68c71e
bbcox27586 <g199703075@gmail.com>
2025-01-09 00:53:42 +0800

    git commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,75 +5,56 @@ import "./assets/style.css";
 const API_BASE = "https://ec-course-api.hexschool.io/v2";
 
 // 請自行替換 API_PATH
-const API_PATH = "";
+const API_PATH = "react-test";
 
-function App() {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
+ const LoginInput = ({handleSubmit , formData , handleInputChange}) => {
+  return(<div className="container login">
+    <div className="row justify-content-center">
+      <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
+      <div className="col-8">
+        <form id="form" className="form-signin" onSubmit={handleSubmit}>
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="username"
+              placeholder="name@example.com"
+              value={formData.username}
+              onChange={handleInputChange}
+              required
+              autoFocus
+            />
+            <label htmlFor="username">Email address</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />
+            <label htmlFor="password">Password</label>
+          </div>
+          <button
+            className="btn btn-lg btn-primary w-100 mt-3"
+            type="submit"
+          >
+            登入
+          </button>
+        </form>
+      </div>
+    </div>
+    <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
+  </div>
+  )
+ }
 
-  const [isAuth, setisAuth] = useState(false);
-  const [products, setProducts] = useState([]);
-  const [tempProduct, setTempProduct] = useState(null);
+ const ProductList = ({checkLogin,tempProduct,products, setTempProduct}) => {
+  return(<div className="container">
 
-  async function checkLogin() {
-    try {
-      const token = document.cookie
-        .split("; ")
-        .find((row) => row.startsWith("hexToken="))
-        ?.split("=")[1];
-      console.log(token);
-      axios.defaults.headers.common.Authorization = token;
-
-      const res = await axios.post(`${API_BASE}/api/user/check`);
-      console.log(res);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  const getData = async () => {
-    try {
-      const response = await axios.get(
-        `${API_BASE}/api/${API_PATH}/admin/products`
-      );
-      setProducts(response.data.products);
-    } catch (err) {
-      console.error(err.response.data.message);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    const { id, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [id]: value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post(`${API_BASE}/admin/signin`, formData);
-      const { token, expired } = response.data;
-      document.cookie = `hexToken=${token};expires=${new Date(expired)};`;
-
-      axios.defaults.headers.common.Authorization = `${token}`;
-
-      getData();
-
-      setisAuth(true);
-    } catch (error) {
-      alert("登入失敗: " + error.response.data.message);
-    }
-  };
-
-  return (
-    <>
-      {isAuth ? (
-        <div className="container">
           <div className="row mt-5">
             <div className="col-md-6">
               <button
@@ -165,49 +146,81 @@ function App() {
               )}
             </div>
           </div>
-        </div>
+
+  </div>)
+}
+
+function App() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [isAuth, setisAuth] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [tempProduct, setTempProduct] = useState(null);
+
+  async function checkLogin() {
+    try {
+      const token = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("hexToken="))
+        ?.split("=")[1];
+      console.log(token);
+      axios.defaults.headers.common.Authorization = token;
+
+      const res = await axios.post(`${API_BASE}/api/user/check`);
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        `${API_BASE}/api/${API_PATH}/admin/products`
+      );
+      setProducts(response.data.products);
+      console.log(response)
+    } catch (err) {
+      console.error(err.response.data.message);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(`${API_BASE}/admin/signin`, formData);
+      const { token, expired } = response.data;
+      document.cookie = `hexToken=${token};expires=${new Date(expired)};`;
+
+      axios.defaults.headers.common.Authorization = `${token}`;
+
+      getData();
+
+      setisAuth(true);
+    } catch (error) {
+      console.log(error)
+      alert("登入失敗: " + error.response.data.message);
+    }
+  };
+
+  return (
+    <>
+      {isAuth ? (
+        <ProductList checkLogin={checkLogin} tempProduct={tempProduct} products={products} setTempProduct={setTempProduct} />
       ) : (
-        <div className="container login">
-          <div className="row justify-content-center">
-            <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
-            <div className="col-8">
-              <form id="form" className="form-signin" onSubmit={handleSubmit}>
-                <div className="form-floating mb-3">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="username"
-                    placeholder="name@example.com"
-                    value={formData.username}
-                    onChange={handleInputChange}
-                    required
-                    autoFocus
-                  />
-                  <label htmlFor="username">Email address</label>
-                </div>
-                <div className="form-floating">
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <label htmlFor="password">Password</label>
-                </div>
-                <button
-                  className="btn btn-lg btn-primary w-100 mt-3"
-                  type="submit"
-                >
-                  登入
-                </button>
-              </form>
-            </div>
-          </div>
-          <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
-        </div>
+        <LoginInput handleSubmit={handleSubmit} formData={formData} handleInputChange={handleInputChange}/>
       )}
     </>
   );
