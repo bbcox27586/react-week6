d3fed9e97315b9ea9755d3200e9c60852b9bebce
bbcox27586 <g199703075@gmail.com>
2025-01-26 13:36:14 +0800

    editor dist

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   //開發中  還是產品路徑
-  base:process.env.NODE_ENV === 'production' ? '/react-week3/' : '/',
+  base:process.env.NODE_ENV === 'production' ? '/react-week4/' : '/',
   plugins: [react()],
   server: {
     hmr: false // 禁用熱重載
